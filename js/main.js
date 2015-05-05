56e23310b273a8bc3172b51f073204cc52d9169e
Kevin Rodrigues <kevrodrigues116@gmail.com>
2015-05-05 09:39:34 +0100

    Added redraw function.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var images = {},
 		totalResources = 6,
 		numResourcesLoaded = 0,
 		fps = 30,
-		context = document.getElementById('canvas').getContext('2d');
+		context = document.getElementById('canvas').getContext('2d'),
+		charX = 245,
+		charY = 185;
 
 loadImage('leftArm');
 loadImage('legs');
@@ -19,10 +21,20 @@ function loadImage (item) {
 	};
 	images[item].src = 'img/' + name + '.png';
 }
-//load our resources..
+
+//load our resources and redraw..
 function resourceLoaded() {
 	numResourcesLoaded += 1;
 	if (numResourcesLoaded === totalResources) {
 		setInterval(redraw, 1000 / fps);
 	}
 }
+
+//redraw canvas..
+function redraw() {
+	var x = charX,
+		y = charY;
+		
+}
+
+
